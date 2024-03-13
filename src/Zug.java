c32072b7395b7d5cf0c655d5c4ab1739c902d147
YoneTheUnforgotten <fchbosbach@gmail.com>
2024-03-13 18:59:54 +0100

    Zug hinzugefügt

---
+++ src/Zug.java
@@ -0,0 +1,26 @@
+
+public class Zug {
+    int zeile;
+    int spalte;
+
+    public Zug(int zeile, int spalte) {
+        this.zeile = zeile;
+        this.spalte = spalte;
+    }
+
+    public int getZeile() {
+        return zeile;
+    }
+
+    public void setZeile(int zeile) {
+        this.zeile = zeile;
+    }
+
+    public int getSpalte() {
+        return spalte;
+    }
+
+    public void setSpalte(int spalte) {
+        this.spalte = spalte;
+    }
+}
